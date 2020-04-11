0816150daf8018425a3d7760895a9e837ab88b59
RyotaBannai <ryotala0528@gmail.com>
2020-04-11 21:01:03 +0900

    loops in scala

--- sub/build.sbt
+++ sub/build.sbt
@@ -1,1 +1,8 @@
 scalaVersion:="2.12.10"
+
+
+scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
+// -deprecation 今後廃止予定
+// -feature  明示的に使用を宣言しないといけない事件的な昨日や注意しなければならない機能を利用している
+// -uncheck 型消去などでパターンマッチが有効に機能しない場合
+// -Xlint その他、望ましい書き方や落とし穴についての情報
---
+++ sub/src/Return.scala
@@ -0,0 +1,28 @@
+object Return {
+  def main(args: Array[String]): Unit={
+    println(indexOf(Array("a", "b", "c"), "c"))
+  }
+  def indexOf(array: Array[String], target: String): Int = {
+    var i = 0
+    while(i<array.length){
+      if(array(i) ==  target) return i
+      i+=1
+    }
+    -1
+  }
+
+  for(x <-1 to 5; y<- 1 until 5){
+    println("x="+x+", y="+y)
+  }
+  for(x<-List("A","B","C")) println(x)
+
+  var myList:List[String] = for(x<-List("A","B","C")) yield { // for-comprehension
+    x+"s"
+  }
+  var result = for(x<-myList if x == "Bs")
+    yield "here->"+x
+
+  for(a<-1 to 1000; b<-1 to 1000; c<-1 to 1000; if a*a == b*b + c*c) yield { // ピタゴラス
+    // println((a,b,c))
+  }
+}

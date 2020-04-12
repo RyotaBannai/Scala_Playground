18358adefd1a8bd96b2bef39c0bbe40842d45b16
RyotaBannai <ryotala0528@gmail.com>
2020-04-12 19:34:30 +0900

    add class practice.

--- sub/src/Abs_Inherit.scala
+++ sub/src/Abs_Inherit.scala
@@ -15,3 +15,17 @@ class Abs_Inherit(override val x:Int, override val y:Int) extends P(x,y){
     println(x, y)
   }
 }
+
+// practice
+abstract class Abs3D{
+  val x:Int
+  val y:Int
+  val z:Int
+}
+class Point3D( val x: Int, val y: Int, val z: Int){}
+/*
+val p = new Point3D(10, 20, 30)
+println(p.x) // 10
+println(p.y) // 20
+println(p.z) // 30
+*/

d803fc736a591b2501a78e6bf450a6541b119329
RyotaBannai <ryotala0528@gmail.com>
2021-05-30 17:24:23 +0900

    add: remove main and use App trait.

--- main/src/SP/Main.scala
+++ main/src/SP/Main.scala
@@ -2,9 +2,8 @@ import ChecksumAccumulator._
 
 // Entry point.
 // when single object doesn't have ..., it's called standalone object.(SP85)
-object Main {
-  def main(args: Array[String]): Unit = {
-    // val acc = new ChecksumAccumulator // NOTE: instance variable はそれぞれ保有
-    println(ChecksumAccumulator.calculate("hello"))
-  }
+object Main extends App {
+  // val acc = new ChecksumAccumulator // NOTE: instance variable はそれぞれ保有
+  println(ChecksumAccumulator.calculate("hello"))
+
 }

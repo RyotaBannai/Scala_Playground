d94754815991e85ca29ec4bdaf6b3dbc903e10ea
RyotaBannai <ryotala0528@gmail.com>
2021-06-08 23:10:32 +0900

    add: pattern matching with case class.

--- projectRoot/src/main/scala/CaseClasses.scala
+++ projectRoot/src/main/scala/CaseClasses.scala
@@ -5,3 +5,17 @@ case class Var(name: String) extends Expr
 case class Num(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
 case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
+
+object CaseClasses {
+
+  /** Simplify Operators.
+    * @example simplifyTop(UnOp("-", UnOp("-", Var("name")))) => Var(name)
+    */
+  // pattern => statement
+  def simplifyTop(expr: Expr): Expr = expr match {
+    case UnOp("-", UnOp("-", e)) => e // 負の負は元のまま
+    case BinOp("+", e, Num(0))   => e // 0 の加算は元のまま
+    case BinOp("*", e, Num(1))   => e // 1 の乗算は元のまま；
+    case _                       => expr // wildcard
+  }
+}

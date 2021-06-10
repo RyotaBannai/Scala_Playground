bba814a80d5babf199a4880607432a9b219afb0d
RyotaBannai <ryotala0528@gmail.com>
2021-06-10 22:28:52 +0900

    add: List, insertion sort.

---
+++ projectRoot/src/main/scala/Isort.scala
@@ -0,0 +1,8 @@
+object Isort {
+  def isort(xs: List[Int]): List[Int] =
+    if (xs.isEmpty) Nil else insert(xs.head, isort(xs.tail))
+
+  def insert(x: Int, xs: List[Int]): List[Int] =
+    if (xs.isEmpty || x <= xs.head) x :: xs
+    else xs.head :: insert(x, xs.tail)
+}

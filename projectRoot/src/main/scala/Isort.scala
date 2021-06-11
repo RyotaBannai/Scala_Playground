581129b8fe48f5689822e085735a3b18f2cde564
RyotaBannai <ryotala0528@gmail.com>
2021-06-11 22:57:56 +0900

    add: merge sort.

--- projectRoot/src/main/scala/Isort.scala
+++ projectRoot/src/main/scala/Isort.scala
@@ -18,3 +18,27 @@ object Isort2 {
     case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
   }
 }
+
+object Msort {
+  /*
+   * @example msort((x: Int, y: Int) => x < y)(List(10,3,1,2))
+   * val intSort = msort((x: Int, y: Int) => x < y)_
+   * intSort(List(5,6,3,1))
+   */
+  def msort[T](less: (T, T) => Boolean)(xs: List[T]): List[T] = {
+    def merge(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
+      case (Nil, _) => ys
+      case (_, Nil) => xs
+      case (x :: xs1, y :: ys1) =>
+        if (less(x, y)) x :: merge(xs1, ys)
+        else y :: merge(xs, ys1)
+    }
+
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs splitAt n
+      merge(msort(less)(ys), msort(less)(zs))
+    }
+  }
+}
---
+++ projectRoot/src/main/scala/ListExp.scala
@@ -0,0 +1,14 @@
+/*
+ * @example append(List(1,2), List(3,4))
+ */
+object ListExp {
+  def append[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case List()   => ys
+    case x :: xs1 => x :: append(xs1, ys)
+  }
+
+  def rev[T](xs: List[T]): List[T] = xs match {
+    case List()   => xs
+    case x :: xs1 => rev(xs1) ::: List(x)
+  }
+}

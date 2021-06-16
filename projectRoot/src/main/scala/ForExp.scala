42c36ac15b05dda93937d877175adae16667aea3
RyotaBannai <ryotala0528@gmail.com>
2021-06-17 00:10:06 +0900

    add: n-queens

--- projectRoot/src/main/scala/ForExp.scala
+++ projectRoot/src/main/scala/ForExp.scala
@@ -24,4 +24,28 @@ object ForExp {
 
   // yield => map, flatMap, withFiler
   // without yield => withFilter, foreach
+
+  // n-queens
+  def queens(n: Int): List[List[(Int, Int)]] = {
+    def isSafe(queen: (Int, Int), queens: List[(Int, Int)]) =
+      queens forall (q => !isCheck(queen, q))
+
+    def isCheck(q1: (Int, Int), q2: (Int, Int)) =
+      q1._1 == q2._1 || q1._2 == q2._2 || (q1._1 - q2._1).abs == (q1._2 - q2._2).abs
+
+    def placeQueens(k: Int): List[List[(Int, Int)]] =
+      if (k == 0)
+        List(List())
+      else
+        for {
+          queens <- placeQueens(k - 1)
+          column <- 1 to n
+          queen = (k, column)
+          if isSafe(queen, queens)
+
+        } yield queen :: queens
+
+    placeQueens(n)
+  }
+
 }

4be86e938d07eb92e04029a8cc3146964bb63147
RyotaBannai <ryotala0528@gmail.com>
2021-06-27 22:58:29 +0900

    add: improved Printable.

--- catsProjctroot/src/main/scala/Printable.scala
+++ catsProjctroot/src/main/scala/Printable.scala
@@ -14,12 +14,23 @@ object PrintableInstances {
     new Printable[Int] {
       def format(value: Int): String = value.toString
     }
+
+  implicit val catFormatter: Printable[Cat] =
+    new Printable[Cat] {
+      def format(value: Cat): String = {
+        val name = Printable.format(value.name)
+        val age = Printable.format(value.age)
+        val color = Printable.format(value.color)
+        s"${name} is a ${age} year-old ${color} cat."
+      }
+    }
 }
 
 // Use
 object Printable {
+  def format[A](value: A)(implicit p: Printable[A]): String = p.format(value)
   def print[A](value: A)(implicit p: Printable[A]): Unit = println(
-    p.format(value)
+    format(value)
   )
 }
 
@@ -31,8 +42,13 @@ import PrintableInstances._
  */
 object PrintableSyntax {
   implicit class PrintableOps[A](value: A) {
-    def print(implicit p: Printable[A]): Unit = println(
-      p.format(value)
-    )
+    def format(implicit p: Printable[A]): String = p.format(value)
+    def print(implicit p: Printable[A]): Unit = println(format(p))
   }
 }
+/*
+val cat = Cat("Dog", 11, "black")
+Printable.print(cat)
+cat.print
+ */
+final case class Cat(name: String, age: Int, color: String)

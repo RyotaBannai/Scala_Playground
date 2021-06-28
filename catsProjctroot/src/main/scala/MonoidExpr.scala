103b2dde2d6e28d067fb3fa60c1ca87d30b4aaf3
RyotaBannai <ryotala0528@gmail.com>
2021-06-28 22:20:31 +0900

    add: Monoids for Set

--- catsProjctroot/src/main/scala/MonoidExpr.scala
+++ catsProjctroot/src/main/scala/MonoidExpr.scala
@@ -19,6 +19,10 @@ trait Semigroup[A] {
   def combine(x: A, y: A): A
 }
 
+object Semigroup {
+  def apply[A](implicit semigroup: Semigroup[A]) = semigroup
+}
+
 trait Monoid[A] extends Semigroup[A] {
   def empty: A
 }
@@ -64,5 +68,41 @@ object BooleanMonoids {
     new Monoid[Boolean] {
       def combine(a: Boolean, b: Boolean) = (a || !b) && (!a || b)
       def empty = true
+    }
+}
+
+object SetMonoids {
+  // We need to define setUnionMonoid as a method rather than a value so we can accept the type parameter A
+  implicit def setUnionMonoids[A]: Monoid[Set[A]] =
+    new Monoid[Set[A]] {
+      def combine(a: Set[A], b: Set[A]) = a union b
+      def empty = Set.empty[A]
+    }
+  /*
+    import SetMonoids._
+    val intSetMonoid = Monoid[Set[Int]]
+    val strSetMonoid = Monoid[Set[String]]
+
+    intSetMonoid.combine(Set(1, 2), Set(2, 3))
+    // res18: Set[Int] = Set(1, 2, 3)
+    strSetMonoid.combine(Set("A", "B"), Set("B", "C"))
+    // res19: Set[String] = Set("A", "B", "C")
+   */
+
+  // no Set complement and set difference
+
+  // symdiff is ok
+  // (Set(1,5) diff Set(1,2,3)) union Set(1,2,3) diff Set(1,5) and
+  // (Set(1,5) diff Set(1,2,3)) union (Set(1,2,3) diff Set(1,5)) are the same.
+  implicit def symDiffMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+    def combine(a: Set[A], b: Set[A]) = (a diff b) union (b diff a)
+    def empty = Set.empty[A]
+  }
+}
+
+object SetSemigroups {
+  implicit def setIntersectionSemigroup[A]: Semigroup[Set[A]] =
+    new Semigroup[Set[A]] {
+      def combine(a: Set[A], b: Set[A]) = a intersect b
     }
 }

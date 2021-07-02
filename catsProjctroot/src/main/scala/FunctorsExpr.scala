32cc485aa458ccbaf6dfde16ba63c3189d7f4148
RyotaBannai <ryotala0528@gmail.com>
2021-07-02 13:32:12 +0900

    add: Contra map

--- catsProjctroot/src/main/scala/FunctorsExpr.scala
+++ catsProjctroot/src/main/scala/FunctorsExpr.scala
@@ -103,3 +103,60 @@ object BranchingOut {
     def leaf[A](value: A): Tree[A] = Leaf(value)
   }
 }
+
+object ContraMapExpr {
+  // Type class
+  trait Printable[A] { self =>
+    def format(value: A): String
+    def contramap[B](func: B => A): Printable[B] =
+      new Printable[B] {
+        def format(value: B): String = self.format(func(value))
+      }
+  }
+
+  final case class Box[A](value: A)
+
+  // Instance
+  object ContraMapPrintableInstances {
+    implicit val stringPrintable: Printable[String] =
+      new Printable[String] {
+        def format(value: String): String = s"'${value}'"
+      }
+
+    implicit val booleanPrintable: Printable[Boolean] =
+      new Printable[Boolean] {
+        def format(value: Boolean): String = if (value) "yes" else "no"
+      }
+
+    // define manually...
+    // implicit def boxPrintable[A](implicit
+    //     p: Printable[A]
+    // ): Printable[Box[A]] =
+    //   new Printable[Box[A]] {
+    //     def format(box: Box[A]): String = p.format(box.value)
+    //   }
+
+    // or use contra map
+    implicit def contraMapBoxPrintable[A](implicit
+        p: Printable[A]
+    ): Printable[Box[A]] = p.contramap[Box[A]](_.value)
+    // first use _.value (B => A: Box => String), then apply overrode format method
+  }
+
+  // Use
+  object ContraMapPrintable {
+    def format[A](value: A)(implicit p: Printable[A]): String = p.format(value)
+    def print[A](value: A)(implicit p: Printable[A]): Unit = println(
+      format(value)
+    )
+  }
+  /*
+    import ContraMapExpr._, ContraMapPrintable._, ContraMapPrintableInstances._
+    format("hello")
+    // String = "'hello'"
+    format(true)
+    // String = "yes"
+    format(Box("hello world")) // Contra map
+    // String = "'hello world'"
+   */
+}

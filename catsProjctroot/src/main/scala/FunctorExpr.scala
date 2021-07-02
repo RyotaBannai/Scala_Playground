bb96dd2d9247a929e1493f8ea88210477119e003
RyotaBannai <ryotala0528@gmail.com>
2021-07-02 18:16:46 +0900

    add: Invariant in Cats

--- catsProjctroot/src/main/scala/FunctorExpr.scala
+++ catsProjctroot/src/main/scala/FunctorExpr.scala
@@ -3,6 +3,11 @@ import cats.syntax.contravariant._
 import cats.Show
 import cats.instances.string._
 
+import cats.Monoid
+import cats.instances.string._ // for Monoid
+import cats.syntax.invariant._ // for imap
+import cats.syntax.semigroup._ // for |+|
+
 /** The simplified version of definitions:
   *
   * trait Contravariant[F[_]] {
@@ -20,10 +25,27 @@ object FunctorExpr {
     Contravariant[Show].contramap(showString)((sym: Symbol) => s"'${sym.name}'")
   // show method for type class instance just as it is.
   showSymbol.show(Symbol("dave"));
+  // Show.show has to be String, but the given is Symbol and before it's consumed by
+  // it's converted from Symbol to String by contravariant.
   // String = 'dave'
 
   // or
   showString
     .contramap[Symbol](sym => s"'${sym.name}'")
     .show(Symbol("dave"))
+
+  implicit val symbolMonoid: Monoid[Symbol] =
+    Monoid[String].imap(Symbol.apply)(_.name)
+
+  Monoid[Symbol].empty
+  Symbol("a") |+| Symbol("few") |+| Symbol("words")
+  /*
+  1. accept two Symbols as parameters;
+  2. convert the Symbols to Strings;
+  3. combine the Strings using Monoid[String];
+  4. convert the result back to a Symbol.
+
+  We can implement combine using imap,
+  passing functions of type String => Symbol and Symbol => String as parameters
+   */
 }

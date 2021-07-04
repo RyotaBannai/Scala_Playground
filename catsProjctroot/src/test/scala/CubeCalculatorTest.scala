cb8aeba3067842a94228cf54449a72f41071c717
RyotaBannai <ryotala0528@gmail.com>
2021-07-04 22:44:12 +0900

    delete: deleted test files not in use.

--- catsProjctroot/src/test/scala/CubeCalculatorTest.scala
+++
@@ -1,5 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-
-class CubeCalculatorTest extends AnyFlatSpec {
-  assert(CubeCalculator.cube(3) === 27)
-}
--- catsProjctroot/src/test/scala/HelloSpec.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.diagrams.Diagrams
-import org.scalatest.flatspec.AnyFlatSpec
-
-class HelloSpec extends AnyFlatSpec with Diagrams {
-  // assert(List(1, 2, 3).contains(4))
-  assertResult(2) {
-    3
-  }
-}

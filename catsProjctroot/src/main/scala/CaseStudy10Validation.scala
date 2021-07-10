a623ff25cb69c25ff39917e49d20a34f150e2bb9
RyotaBannai <ryotala0528@gmail.com>
2021-07-10 19:34:42 +0900

    add: use Validation.

--- catsProjctroot/src/main/scala/CaseStudy10Validation.scala
+++ catsProjctroot/src/main/scala/CaseStudy10Validation.scala
@@ -124,4 +124,30 @@ object CaseStudy10Validation {
     error(s"Must contain the character $char only once"),
     str => str.filter(c => c == char).size == 1
   )
+
+  val checkUsername: Check[Errors, String, String] = Check(
+    longerThan(3) and alphanumeric
+  )
+
+  val splitEmail: Check[Errors, String, (String, String)] = Check(
+    _.split('@') match {
+      case Array(name, domain) => (name, domain).validNel[String]
+      case _                   => "Most contain a single @ character".invalidNel[(String, String)]
+    }
+  )
+
+  val checkLeft: Check[Errors, String, String] = Check(longerThan(0))
+  val checkRight: Check[Errors, String, String] = Check(
+    longerThan(3) and contains('.')
+  )
+  val joinEmail: Check[Errors, (String, String), String] = Check {
+    case (l, r) => (checkLeft(l), checkRight(r)).mapN(_ + "@" + _)
+  }
+
+  val checkEmail: Check[Errors, String, String] = splitEmail andThen joinEmail
+
+  final case class User(username: String, email: String)
+
+  def createUser(username: String, email: String): Validated[Errors, User] =
+    (checkUsername(username), checkEmail(email)).mapN(User)
 } // end of CaseStudy10Validation object

4cca613b49a17bdb72c5ce18ee290dbe014114a3
RyotaBannai <ryotala0528@gmail.com>
2021-07-17 16:01:52 +0900

    add: added DeviceGroupQuery which interacts with DeviceGroup on behalf of DeviceManager.

--- akkaProjctroot/src/main/scala/IotSupervisor.scala
+++ akkaProjctroot/src/main/scala/IotSupervisor.scala
@@ -84,11 +84,11 @@ object DeviceManager {
 
 object DeviceGroupQuery {
   def apply(
-      deviceIdActor: Map[String, ActorRef[Device.Command]],
+      deviceIdToActor: Map[String, ActorRef[Device.Command]],
       requestId: Long,
       requester: ActorRef[DeviceManager.RespondAllTemperatures],
       timeout: FiniteDuration
-  ) {
+  ): Behavior[DeviceGroupQuery.Command] = {
     Behaviors.setup { context =>
       Behaviors.withTimers { timers =>
         new DeviceGroupQuery(deviceIdToActor, requestId, requester, timeout, context, timers)
@@ -103,7 +103,7 @@ object DeviceGroupQuery {
 } // end of object DeviceGroupQuery
 
 class DeviceGroupQuery(
-    deviceIdActor: Map[String, ActorRef[Device.Command]],
+    deviceIdToActor: Map[String, ActorRef[Device.Command]],
     requestId: Long,
     requester: ActorRef[DeviceManager.RespondAllTemperatures],
     timeout: FiniteDuration,
@@ -111,21 +111,68 @@ class DeviceGroupQuery(
     timers: TimerScheduler[DeviceGroupQuery.Command]
 ) extends AbstractBehavior[DeviceGroupQuery.Command](context) {
   import DeviceGroupQuery._
-  import DevieManaer.DeviceNotAvailable
-  import DevieManaer.DeviceTimedOut
-  import DevieManaer.RespondAllTemperatures
-  import DevieManaer.Temperature
-  import DevieManaer.TemperatureNotAvailable
-  import DevieManaer.TemperatureReading
+  import DeviceManager.DeviceNotAvailable
+  import DeviceManager.DeviceTimedOut
+  import DeviceManager.RespondAllTemperatures
+  import DeviceManager.Temperature
+  import DeviceManager.TemperatureNotAvailable
+  import DeviceManager.TemperatureReading
 
   timers.startSingleTimer(CollectionTimeout, CollectionTimeout, timeout)
   private val respondTemperatureAdapter = context.messageAdapter(WrappedRespondTemperature.apply)
 
-  deviceIdActor.foreach { case (deviceId, device) =>
+  deviceIdToActor.foreach { case (deviceId, device) =>
     context.watchWith(device, DeviceTerminated(deviceId))
     device ! Device.ReadTemperature(0, respondTemperatureAdapter)
   }
 
+  private var repliesSoFar = Map.empty[String, TemperatureReading]
+  private var stillWaiting = deviceIdToActor.keySet
+
+  override def onMessage(msg: Command): Behavior[Command] =
+    msg match {
+      // Device.RespondTemperature
+      case WrappedRespondTemperature(response) => onRespondTemperature(response)
+      case DeviceTerminated(deviceId)          => onDeviceTerminated(deviceId)
+      case CollectionTimeout                   => onCollectionTimeout()
+    }
+
+  private def onRespondTemperature(response: Device.RespondTemperature): Behavior[Command] = {
+    val reading = response.value match {
+      case Some(value) => Temperature(value)
+      case None        => TemperatureNotAvailable
+    }
+
+    val deviceId = response.deviceId
+    repliesSoFar += (deviceId -> reading)
+    stillWaiting -= deviceId
+
+    respondWhenAllCollected()
+  }
+
+  private def onDeviceTerminated(deviceId: String): Behavior[Command] = {
+    if (stillWaiting(deviceId)) {
+      repliesSoFar += (deviceId -> DeviceNotAvailable)
+      stillWaiting -= deviceId
+    }
+    respondWhenAllCollected()
+  }
+
+  private def onCollectionTimeout(): Behavior[Command] = {
+    repliesSoFar ++= stillWaiting.map(deviceId => deviceId -> DeviceTimedOut)
+    stillWaiting = Set.empty
+    respondWhenAllCollected()
+  }
+
+  /** If there is any remaining actor we are waiting for it,
+    * otherwise we send the result of the query to the original requester and stop the query actor(myself).
+    */
+  private def respondWhenAllCollected(): Behavior[Command] = {
+    if (stillWaiting.isEmpty) {
+      requester ! DeviceManager.RespondAllTemperatures(requestId, repliesSoFar)
+      Behaviors.stopped
+    } else { this }
+  }
 } // end of class DeviceGroupQuery
 
 class DeviceManager(context: ActorContext[DeviceManager.Command])
@@ -247,7 +294,7 @@ object Device {
   // the device actor will use ActorRef when the device actor will use when replaying to the request
   final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature])
       extends Command
-  final case class RespondTemperature(requestId: Long, value: Option[Double])
+  final case class RespondTemperature(requestId: Long, deviceId: String, value: Option[Double])
 
   // to receive
   final case class RecordTemperature(
@@ -272,7 +319,7 @@ class Device(context: ActorContext[Device.Command], groupId: String, deviceId: S
   override def onMessage(msg: Command): Behavior[Command] = {
     msg match {
       case ReadTemperature(id, replyTo) =>
-        replyTo ! RespondTemperature(id, lastTemperatureReading)
+        replyTo ! RespondTemperature(id, deviceId, lastTemperatureReading)
         this
 
       case RecordTemperature(id, value, replyTo) =>
